fb0e1a8901511c80e2bd3d8c7df283fa950d5e69
Talley Lambert <talley.lambert@gmail.com>
2021-04-24 12:00:53 -0400

    fix pyside2

--- pyqrangeslider/qtcompat/QtCore.py
+++ pyqrangeslider/qtcompat/QtCore.py
@@ -16,7 +16,6 @@ from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError
 if PYQT5:
     from PyQt5.QtCore import QT_VERSION_STR as __version__
     from PyQt5.QtCore import *
-    from PyQt5.QtCore import pyqtBoundSignal as SignalInstance  # noqa
     from PyQt5.QtCore import pyqtProperty as Property  # noqa
     from PyQt5.QtCore import pyqtSignal as Signal  # noqa
     from PyQt5.QtCore import pyqtSlot as Slot  # noqa
@@ -24,9 +23,8 @@ if PYQT5:
     # Those are imported from `import *`
     del pyqtSignal, pyqtBoundSignal, pyqtSlot, pyqtProperty, QT_VERSION_STR
 elif PYQT6:
+    from PyQt5.QtCore import *
     from PyQt6.QtCore import QT_VERSION_STR as __version__
-    from PyQt6.QtCore import *
-    from PyQt6.QtCore import pyqtBoundSignal as SignalInstance  # noqa
     from PyQt6.QtCore import pyqtProperty as Property  # noqa
     from PyQt6.QtCore import pyqtSignal as Signal  # noqa
     from PyQt6.QtCore import pyqtSlot as Slot  # noqa
@@ -46,10 +44,12 @@ elif PYQT6:
     del pyqtSignal, pyqtBoundSignal, pyqtSlot, pyqtProperty, QT_VERSION_STR
 elif PYSIDE2:
     import PySide2.QtCore
+    from PySide2.QtCore import *  # noqa
 
     __version__ = PySide2.QtCore.__version__
 elif PYSIDE6:
     import PySide6.QtCore
+    from PySide6.QtCore import *  # noqa
 
     __version__ = PySide6.QtCore.__version__
 
