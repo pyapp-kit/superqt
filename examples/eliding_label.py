caf6cd97686da70ca27144c0984e187049de24e7
Talley Lambert <talley.lambert@gmail.com>
2021-08-16 19:54:24 -0400

    wrap

--- examples/eliding_label.py
+++ examples/eliding_label.py
@@ -4,7 +4,8 @@ from superqt.qtcompat.QtWidgets import QApplication
 app = QApplication([])
 
 labl = QElidingLabel(
-    "a skj skjfskfj sdlf sdfl sdlfk jsdf sdlkf jdsf dslfksdlfk sdlfk sdf sdl fjsdlf kjsdlfk jsldkfjsdlkfj sdlkfj"
+    "a skj skjfskfj sdlf sdfl sdlfk jsdf sdlkf jdsf dslfksdl sdlfk sdf sdl "
+    "fjsdlf kjsdlfk laskdfsal as lsdfjdsl kfjdslf asfd dslkjfldskf sdlkfj"
 )
 labl.setWordWrap(True)
 labl.show()
