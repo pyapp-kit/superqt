9dbaa714c62977d87dc0bbbb3601c1ae6d587866
Grzegorz Bokota <bokota+github@gmail.com>
2021-09-08 11:54:54 +0200

    add test for property

--- superqt/utils/_tests/test_ensure_thread.py
+++ superqt/utils/_tests/test_ensure_thread.py
@@ -11,9 +11,35 @@ class SampleObject(QObject):
         super().__init__()
         self.main_thread_res = {}
         self.object_thread_res = {}
+        self.main_thread_prop_val = None
+        self.sample_thread_prop_val = None
 
     def long_wait(self):
         time.sleep(1)
+
+    @property
+    def sample_main_thread_property(self):
+        return self.main_thread_prop_val
+
+    @sample_main_thread_property.setter
+    @ensure_main_thread()
+    def sample_main_thread_property(self, value):
+        if QThread.currentThread() is not QCoreApplication.instance().thread():
+            raise RuntimeError("Wrong thread")
+        self.main_thread_prop_val = value
+        self.assigment_done.emit()
+
+    @property
+    def sample_object_thread_property(self):
+        return self.sample_thread_prop_val
+
+    @sample_object_thread_property.setter
+    @ensure_object_thread()
+    def sample_object_thread_property(self, value):
+        if QThread.currentThread() is not self.thread():
+            raise RuntimeError("Wrong thread")
+        self.sample_thread_prop_val = value
+        self.assigment_done.emit()
 
     @ensure_main_thread()
     def check_main_thread(self, a, *, b=1):
@@ -50,6 +76,7 @@ class LocalThread(QThread):
     def run(self):
         assert QThread.currentThread() is not QCoreApplication.instance().thread()
         self.ob.check_main_thread(5, b=8)
+        self.ob.main_thread_prop_val = "text2"
 
 
 class LocalThread2(QThread):
@@ -70,6 +97,10 @@ def test_only_main_thread(qapp):
     assert ob.main_thread_res == {"a": 1, "b": 3}
     ob.check_object_thread(2, b=4)
     assert ob.object_thread_res == {"a": 2, "b": 4}
+    ob.sample_main_thread_property = 5
+    assert ob.sample_main_thread_property == 5
+    ob.sample_object_thread_property = 7
+    assert ob.sample_object_thread_property == 7
 
 
 def test_object_thread(qtbot):
@@ -79,18 +110,24 @@ def test_object_thread(qtbot):
     ob.moveToThread(thread)
     with qtbot.waitSignal(ob.assigment_done):
         ob.check_object_thread(2, b=4)
+    assert ob.object_thread_res == {"a": 2, "b": 4}
+
+    with qtbot.waitSignal(ob.assigment_done):
+        ob.sample_object_thread_property = "text"
+
+    assert ob.sample_object_thread_property == "text"
     assert ob.thread() is thread
     thread.exit(0)
-    assert ob.object_thread_res == {"a": 2, "b": 4}
 
 
 def test_main_thread(qtbot):
     ob = SampleObject()
     t = LocalThread(ob)
-    with qtbot.waitSignal(ob.assigment_done, timeout=10000000):
+    with qtbot.waitSignal(t.finished, timeout=10000000):
         t.start()
 
     assert ob.main_thread_res == {"a": 5, "b": 8}
+    assert ob.sample_main_thread_property == "text2"
 
 
 def test_object_thread_return(qtbot):
