19ebe0a875a57f78a8e3d13a8c805486978ce321
Talley Lambert <talley.lambert@gmail.com>
2021-10-13 16:40:10 -0400

    use sigint after time

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import multiprocessing as mp
+import os
+import signal
+
+import pytest
+
+
+def wait_sigint(q: mp.Queue, pid):
+    try:
+        print("waiting get q")
+        q.get(timeout=2 * 60)
+    except Exception:
+        print("Timeout")
+        os.kill(pid, signal.SIGINT)
+        import time
+
+        time.sleep(30)
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            pass
+        else:
+            os.kill(pid, signal.SIGKILL)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def sigint_after_time():
+    manager = mp.Manager()
+    q = manager.Queue()
+    p = mp.Process(target=wait_sigint, args=(q, os.getpid()))
+    p.start()
+    yield
+    q.put(1)
+    p.join()
