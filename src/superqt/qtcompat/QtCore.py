df43e3d8901be94a2ab46552781616b3c6e27608
Talley Lambert <talley.lambert@gmail.com>
2021-10-12 23:31:00 -0400

    undo change

--- src/superqt/qtcompat/QtCore.py
+++ src/superqt/qtcompat/QtCore.py
@@ -15,7 +15,6 @@ from . import PYQT5, PYQT6, PYSIDE2, PYSIDE6, PythonQtError
 if PYQT5:
     from PyQt5.QtCore import QT_VERSION_STR as __version__
     from PyQt5.QtCore import *
-    from PyQt5.QtCore import pyqtBoundSignal as SignalInstance  # noqa
     from PyQt5.QtCore import pyqtProperty as Property  # noqa
     from PyQt5.QtCore import pyqtSignal as Signal  # noqa
     from PyQt5.QtCore import pyqtSlot as Slot  # noqa
@@ -23,7 +22,6 @@ if PYQT5:
     # Those are imported from `import *`
     del pyqtSignal, pyqtBoundSignal, pyqtSlot, pyqtProperty, QT_VERSION_STR
 elif PYQT6:
-    from PyQt5.QtCore import pyqtBoundSignal as SignalInstance  # noqa
     from PyQt6.QtCore import QT_VERSION_STR as __version__
     from PyQt6.QtCore import *
     from PyQt6.QtCore import pyqtProperty as Property  # noqa
