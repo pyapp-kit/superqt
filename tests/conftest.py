a12726c5640ad5117c182b0bb3bd37e55c508a10
Talley Lambert <talley.lambert@gmail.com>
2021-10-14 10:27:50 -0400

    timtout

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,36 @@
-# import multiprocessing as mp
-# import os
-# import signal
-# import time
+import multiprocessing as mp
+import os
+import signal
+import time
 
-# import pytest
-
-
-# def wait_sigint(q: mp.Queue, pid):
-#     try:
-#         q.get(timeout=10)
-#     except Exception:
-#         print("Timeout")
-#         os.kill(pid, signal.SIGINT)
-
-#         time.sleep(30)
-#         try:
-#             os.kill(pid, 0)
-#         except OSError:
-#             pass
-#         else:
-#             os.kill(pid, signal.SIGKILL)
+import pytest
 
 
-# @pytest.fixture(scope="function", autouse=True)
-# def sigint_after_time():
-#     start = time.perf_counter()
-#     manager = mp.Manager()
-#     q = manager.Queue()
-#     p = mp.Process(target=wait_sigint, args=(q, os.getpid()))
-#     p.start()
-#     yield
-#     q.put(1)
-#     p.join()
-#     end = time.perf_counter()
-#     print("time: ", end - start)
+def wait_sigint(q: mp.Queue, pid):
+    try:
+        q.get(timeout=20)
+    except Exception:
+        print("Timeout")
+        os.kill(pid, signal.SIGINT)
+
+        time.sleep(30)
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            pass
+        else:
+            os.kill(pid, signal.SIGKILL)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def sigint_after_time():
+    start = time.perf_counter()
+    manager = mp.Manager()
+    q = manager.Queue()
+    p = mp.Process(target=wait_sigint, args=(q, os.getpid()))
+    p.start()
+    yield
+    q.put(1)
+    p.join()
+    end = time.perf_counter()
+    print("time: ", end - start)
