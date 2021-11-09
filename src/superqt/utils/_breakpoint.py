132f8b9b0de01bb798b4318c099cafabfd7a024f
Talley Lambert <talley.lambert@gmail.com>
2021-11-08 21:16:58 -0500

    update docs

--- src/superqt/utils/_breakpoint.py
+++ src/superqt/utils/_breakpoint.py
@@ -7,7 +7,7 @@ from superqt.qtcompat import QtCore
 from superqt.qtcompat.QtWidgets import QApplication
 
 
-class QtPdb(pdb.Pdb):
+class _QtPdb(pdb.Pdb):
     def set_trace(self, frame: Optional[FrameType] = None) -> None:
         QApplication.processEvents()
         if hasattr(QtCore, "pyqtRemoveInputHook"):
@@ -27,15 +27,26 @@ class QtPdb(pdb.Pdb):
 
 
 def qt_set_trace(*, header: Optional[str] = None) -> None:
-    pdb = QtPdb()
+    """Enter the debugger at the calling stack frame, with Qt event loop removed.
+
+    This is useful to hard-code a breakpoint at a given point in a program,
+    even if the code is not otherwise being debugged (e.g. when an assertion fails).
+    If given, header is printed to the console just before debugging begins.
+    """
+    pdb = _QtPdb()
     if header is not None:
         pdb.message(header)
     pdb.set_trace(sys._getframe().f_back)
 
 
 def install_qt_breakpoint() -> None:
+    """Install `qt_set_trace` as the default breakpointhook.
+
+    This lets you use the builtin `breakpoint()` anywhere in your Qt code.
+    """
     sys.breakpointhook = qt_set_trace
 
 
 def uninstall_qt_breakpoint() -> None:
+    """Uninstall `qt_set_trace` as the default breakpointhook."""
     sys.breakpointhook = sys.__breakpointhook__
