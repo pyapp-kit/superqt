0ae3350c5796a1328f4c45eaac095035fcadee84
Grzegorz Bokota <bokota+github@gmail.com>
2022-05-18 22:50:51 +0200

    Add code syntax highlight utils (#88)
    * add code syntax highlight code
    
    * add example
    
    * add documentation and fix example
    
    * add tests
    
    * add information about napari theme usage
    
    * clean napari mention

---
+++ docs/utils.md
@@ -0,0 +1,10 @@
+# Utils
+
+## Code highlighting
+
+`superqt` provides a code highlighter subclass of `QSyntaxHighlighter`
+that can be used to highlight code in a QTextEdit.
+
+Code lexer and available styles are from [`pygments`](https://pygments.org/) python library
+List of available languages are available [here](https://pygments.org/languages/).
+List of available styles are available [here](https://pygments.org/styles/).
---
+++ examples/code_highlight.py
@@ -0,0 +1,32 @@
+from PyQt5.QtGui import QColor, QPalette
+from qtpy.QtWidgets import QApplication, QTextEdit
+
+from superqt.utils import CodeSyntaxHighlight
+
+app = QApplication([])
+
+text_area = QTextEdit()
+
+highlight = CodeSyntaxHighlight(text_area.document(), "python", "monokai")
+
+palette = text_area.palette()
+palette.setColor(QPalette.Base, QColor(highlight.background_color))
+text_area.setPalette(palette)
+text_area.setText(
+    """from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("name", help="Your name")
+    args = parser.parse_args()
+    print(f"Hello {args.name}")
+
+
+if __name__ == "__main__":
+    main()
+"""
+)
+
+text_area.show()
+
+app.exec_()
