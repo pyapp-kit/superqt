3f64d8aa76c981cb9eec5c4edc5eea6ff2a54b60
Talley Lambert <talley.lambert@gmail.com>
2022-10-03 14:42:06 -0400

    test: more tests

--- tests/test_sliders/test_labeled_slider.py
+++ tests/test_sliders/test_labeled_slider.py
@@ -1,6 +1,9 @@
+from typing import Any, Iterable
 from unittest.mock import Mock
 
-from superqt import QLabeledRangeSlider, QLabeledSlider
+import pytest
+
+from superqt import QLabeledDoubleSlider, QLabeledRangeSlider, QLabeledSlider
 
 
 def test_labeled_slider_api(qtbot):
@@ -20,37 +23,40 @@ def test_slider_connect_works(qtbot):
     slider._label.editingFinished.emit()
 
 
-def _assert_types(args, type_):
+def _assert_types(args: Iterable[Any], type_: type):
     # sourcery skip: comprehension-to-generator
     assert all([isinstance(v, type_) for v in args]), "invalid type"
 
 
-def test_labeled_signals(qtbot):  # sourcery skip: comprehension-to-generator
-    ls = QLabeledSlider()
-    qtbot.addWidget(ls)
+@pytest.mark.parametrize("cls", [QLabeledDoubleSlider, QLabeledSlider])
+def test_labeled_signals(cls, qtbot):
+    gslider = cls()
+    qtbot.addWidget(gslider)
+
+    type_ = float if cls == QLabeledDoubleSlider else int
 
     mock = Mock()
-    ls.valueChanged.connect(mock)
-    with qtbot.waitSignal(ls.valueChanged):
-        ls.setValue(10)
+    gslider.valueChanged.connect(mock)
+    with qtbot.waitSignal(gslider.valueChanged):
+        gslider.setValue(10)
     mock.assert_called_once_with(10)
-    assert all([isinstance(v, int) for v in mock.call_args.args]), "invalid type"
+    _assert_types(mock.call_args.args, type_)
 
     mock = Mock()
-    ls.rangeChanged.connect(mock)
-    with qtbot.waitSignal(ls.rangeChanged):
-        ls.setMinimum(3)
+    gslider.rangeChanged.connect(mock)
+    with qtbot.waitSignal(gslider.rangeChanged):
+        gslider.setMinimum(3)
     mock.assert_called_once_with(3, 99)
-    _assert_types(mock.call_args.args, int)
+    _assert_types(mock.call_args.args, type_)
 
     mock.reset_mock()
-    with qtbot.waitSignal(ls.rangeChanged):
-        ls.setMaximum(15)
+    with qtbot.waitSignal(gslider.rangeChanged):
+        gslider.setMaximum(15)
     mock.assert_called_once_with(3, 15)
-    _assert_types(mock.call_args.args, int)
+    _assert_types(mock.call_args.args, type_)
 
     mock.reset_mock()
-    with qtbot.waitSignal(ls.rangeChanged):
-        ls.setRange(1, 2)
+    with qtbot.waitSignal(gslider.rangeChanged):
+        gslider.setRange(1, 2)
     mock.assert_called_once_with(1, 2)
-    _assert_types(mock.call_args.args, int)
+    _assert_types(mock.call_args.args, type_)
