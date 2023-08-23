b5cdd22de43fc53fd5b2c92c7e33e1d14cebadd7
Talley Lambert <talley.lambert@gmail.com>
2023-08-22 21:14:32 -0400

    Deployed 599dff7 with MkDocs version: 1.5.2

--- _macros.py
+++ _macros.py
@@ -37,7 +37,7 @@ def define_env(env: "MacrosPlugin"):
             )
             src = src.replace("app.exec_()", "")
 
-            exec(src)
+            exec(src)  # noqa: S102
             _grab(dest, width)
         return (
             f"![{page.title}](../{dest.parent.name}/{dest.name})"
@@ -104,7 +104,6 @@ def define_env(env: "MacrosPlugin"):
                     out += f"- `{m.name}`\n\n"
 
         if self_members:
-
             out += dedent(
                 f"""
             ## Methods
