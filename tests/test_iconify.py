969447ea3b70f2581caba2a9dffa5ee9b7f9c490
Talley Lambert <talley.lambert@gmail.com>
2023-09-29 21:49:08 -0400

    importorskip

--- tests/test_iconify.py
+++ tests/test_iconify.py
@@ -1,9 +1,11 @@
+import pytest
 from qtpy.QtWidgets import QPushButton
 
 from superqt import QIconifyIcon
 
 
 def test_qiconify(qtbot):
+    pytest.importorskip("pyconify")
     icon = QIconifyIcon("bi:alarm-fill", color="red", rotate=90)
     assert icon.path.endswith(".svg")
     btn = QPushButton()
