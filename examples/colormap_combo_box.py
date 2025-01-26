eaaa7ebfc6f551c22fa351784d4ff05279e2b0a6
Peter Sobolewski <pete.sd@gmail.com>
2025-01-26 13:53:39 -0500

    update example to show the new class

--- examples/colormap_combo_box.py
+++ examples/colormap_combo_box.py
@@ -1,6 +1,6 @@
 from qtpy.QtWidgets import QApplication, QVBoxLayout, QWidget
 
-from superqt.cmap import CmapCatalogComboBox, QColormapComboBox
+from superqt.cmap import CmapCatalogComboBox, QColormapComboBox, QColormapFilterComboBox
 
 app = QApplication([])
 
@@ -9,11 +9,17 @@ layout = QVBoxLayout(wdg)
 
 catalog_combo = CmapCatalogComboBox(interpolation="linear")
 
+selected_cmaps = ["viridis", "plasma", "magma", "inferno", "turbo"]
+
 selected_cmap_combo = QColormapComboBox(allow_user_colormaps=True)
-selected_cmap_combo.addColormaps(["viridis", "plasma", "magma", "inferno", "turbo"])
+selected_cmap_combo.addColormaps(selected_cmaps)
+
+filter_cmap_combo = QColormapFilterComboBox(allow_user_colormaps=True)
+filter_cmap_combo.addColormaps(selected_cmaps)
 
 layout.addWidget(catalog_combo)
 layout.addWidget(selected_cmap_combo)
+layout.addWidget(filter_cmap_combo)
 
 wdg.show()
 app.exec()
