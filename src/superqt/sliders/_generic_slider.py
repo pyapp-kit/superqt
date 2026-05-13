2e360ad0a306553275cf5e78bc321a15c7cfe65b
Lorenzo Gaifas <brisvag@gmail.com>
2026-05-13 15:15:46 +0200

    add comment

--- src/superqt/sliders/_generic_slider.py
+++ src/superqt/sliders/_generic_slider.py
@@ -415,6 +415,8 @@ class _GenericSlider(QSlider):
         self._pressedControl = SC_HANDLE
 
     def _draw_handle(self, painter, opt):
+        # note that SC_GROOVE here is necessary for add-page and -sub-page to
+        # be styled correctly, otherwise they are ignored
         opt.subControls = SC_HANDLE | SC_GROOVE
         if self._pressedControl:
             opt.activeSubControls = self._pressedControl
